eaffb15014bdbc01f1ed99cca72313fd57067fcb
haddis69 <1474154683@qq.com>
2022-05-27 15:35:20 +0800

    HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const ESLintWebpackPlugin = require("eslint-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     //入口
     entry: './src/main.js',
@@ -81,6 +82,11 @@ module.exports = {
             //配置文件名是固定的几种形式，一定要写在根目录中
             context: path.resolve(__dirname, "src"),
         }),
+        new HtmlWebpackPlugin({
+            // 以 public/index.html 为模板创建文件
+            // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
+            template: path.resolve(__dirname, "public/index.html"),
+        })
     ],
     //模式
     mode: 'development'
